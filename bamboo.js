71de2873aa3060836ef89ad83af387a2cf856e29
hridoykhanjoy32 <74468998+hridoykhanjoy32@users.noreply.github.com>
2020-11-30 02:31:50 +0600

    Update bamboo.js

--- bamboo.js
+++ bamboo.js
@@ -87,10 +87,54 @@ location.href = "https://foldeoxft9myz1jsw6as1a-on.drv.tw/HakingApps/FF_Bamboo_h
   }
   
    
-   
-   function showb() {  
-	  
-document.getElementById("code").style.display = "block";
+ var myTimer;   
+
+  // This code loads the IFrame Player API code asynchronously.
+  var tag = document.createElement("script");
+  tag.src = "//www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName("script")[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  // This function creates an <iframe> (and YouTube player)
+  // after the API code downloads.
+  var player;
+  window.onYouTubeIframeAPIReady = function() {
+    player = new YT.Player("player", {
+      "height": "315",
+      "width": "560",
+      "videoId": "9JeQpJwXik0",
+      playerVars: {
+            controls: 0,
+            disablekb: 1
+        },
+      "events": {
+        "onReady": onPlayerReady,
+        "onStateChange": onPlayerStateChange
+      }
+    });
+  }
+
+      // 4. The API will call this function when the video player is ready.
+      function onPlayerReady(event) {
+        event.target.playVideo();
+        
+      }
+    function onPlayerStateChange(event){
+        if(event.data==1) { // playing
+            myTimer = setInterval(function(){ 
+                var time;
+                time = player.getCurrentTime();
+                $("#timeHolder").text(time);
+                
+                if(time>5){
+                
+                $("#watchtime").text("redy");
+			
+			
+			
+			
+			
+		document.getElementById("code").style.display = "block";
 
     var d = new Date();
          var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
@@ -114,7 +158,65 @@ document.getElementById("code").style.display = "block";
 	  date : Y5,
 	  ip : ip,
 	  downlod: downlod,
-  });
+  });	
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+                
+                }
+                
+                else if(time>4){
+                
+                $("#watchtime").text("Wait 1 sec");
+                
+                }
+                else if(time>3){
+                
+                $("#watchtime").text("Wait 2 sec");
+                
+                }
+                else if(time>2){
+                
+                $("#watchtime").text("Wait 3 sec");
+                
+                }
+                
+                else if(time>1){
+                
+                $("#watchtime").text("Wait 4 sec");
+                
+                }
+                
+            else{
+            
+            $("#watchtime").text("Play video");
+            
+            }    
+                
+            }, 100);
+        }
+        else { // not playing
+            clearInterval(myTimer);
+        }
+    }
+    
+
+
+
+   
+   function showb() {  
+	  
+
     
   }
   
